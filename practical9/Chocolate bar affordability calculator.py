c8cdc8e1c91583d9ad0c58a5f78f8d8219433efd
Kevinsbrew <101186567+Kevinsbrew@users.noreply.github.com>
2022-05-23 17:07:44 +0800

    Delete practical9 directory

--- practical9/Chocolate bar affordability calculator.py
+++
@@ -1,6 +0,0 @@
-def choco (total_money,price):
-    change_left=total_money%price
-    bars=(total_money-change_left)/price
-    print('bars=',bars,'change left =',change_left)
-    return ''
-print(choco(50.5,3))
