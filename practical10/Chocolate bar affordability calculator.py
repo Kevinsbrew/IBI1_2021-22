3ddb2120de2bf8584e07db37e1d3b4c394ada26a
Kevinsbrew <101186567+Kevinsbrew@users.noreply.github.com>
2022-05-24 22:29:42 +0800

    Update Chocolate bar affordability calculator.py

--- practical10/Chocolate bar affordability calculator.py
+++ practical10/Chocolate bar affordability calculator.py
@@ -1,6 +1,7 @@
 def choco (total_money,price):
     change_left=total_money%price
     bars=(total_money-change_left)/price
-    print('bars=',bars,'change left =',change_left)
-    return ''
-print(choco(50.5,3))
+    return  print('bars=',bars,'change left =',change_left)
+
+# example shows we have 50.5 yuan, the price of chocolate is 3 yuan per bar
+choco(50.5,3)
